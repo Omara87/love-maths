9392d47de08a808c3bf8fc493efad3e87c730e93
Omara87 <>
2023-09-11 17:46:24 +0000

    Add event listerners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,49 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listerners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = docucment.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
 
 }
 
-function checkAnswer () {
+function checkAnswer() {
 
 }
 
-function calculateCorrectAnswer () {
+function calculateCorrectAnswer() {
 
 }
 
-function incrementScore () {
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer () {
-
-} 
-
-function displayAdditionQuestion () {
+function incrementWrongAnswer() {
 
 }
 
-function displaySubtractQuestion () {
+function displayAdditionQuestion() {
 
-} 
+}
 
-function displayMultiplyQuestion () {
-    
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
 }
